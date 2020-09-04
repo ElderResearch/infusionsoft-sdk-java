164ba1dcf84489ebcb5b1d73f468058ca2b689ba
Bryce Pilcher <bryce.pilcher@datamininglab.com>
2020-09-04 13:15:00 -0400

    fix deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,10 @@
 REM Checking this in to easily edit and reuse in the future
 mvn deploy:deploy-file^
-  -Dfile="infusionsoft-api-eri-1.0.3.jar"^
-  -Dsources="infusionsoft-api-eri-1.0.3-sources.jar"^
-  -DpomFile="infusionsoft-api-eri-1.0.3.pom"^
+  -Dfile="infusionsoft-api-1.0.3-eri.jar"^
+  -Dsources="infusionsoft-api-1.0.3-eri-sources.jar"^
+  -DpomFile="infusionsoft-api-1.0.3-eri.pom"^
   -DgroupId="com.infusionsoft"^
   -DartifactId="infusionsoft-api"^
-  -Dversion="eri-1.0.3"^
+  -Dversion="1.0.3-eri"^
   -DrepositoryId="eri-releases"^
   -Durl="https://elderresearch.jfrog.io/elderresearch/libs-release"
